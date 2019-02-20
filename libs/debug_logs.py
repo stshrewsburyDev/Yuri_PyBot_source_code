ac486c44f7399cb00c409df4e82f8729813ef7b4
Steven Shrewsbury <39103023+stshrewsburyDev@users.noreply.github.com>
2019-02-20 14:39:40 +0000

    Update debug_logs.py

--- libs/debug_logs.py
+++ libs/debug_logs.py
@@ -5,20 +5,20 @@ Made By Steven Shrewsbury (AKA: stshrewsburyDev)
 
 def DEBUG(log):
     DEBUG_LOG = "[DEBUG] "
-    DEBUG_LOG += log.title() + "\n"
+    DEBUG_LOG += log + "\n"
     print(DEBUG_LOG)
 
 def LOG(log):
     DEBUG_LOG = "[LOG] "
-    DEBUG_LOG += log.title()
+    DEBUG_LOG += log
     print(DEBUG_LOG)
 
 def INFO(log):
     DEBUG_LOG = "[INFO] "
-    DEBUG_LOG += log.title() + "\n"
+    DEBUG_LOG += log + "\n"
     print(DEBUG_LOG)
 
 def ERROR(log):
     DEBUG_LOG = "[ERROR] "
-    DEBUG_LOG += log.title()
+    DEBUG_LOG += log
     print(DEBUG_LOG)
