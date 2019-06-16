db9e167cf48322e8db510d706245cc18618d33e0
Steven Shrewsbury <39103023+stshrewsburyDev@users.noreply.github.com>
2019-06-16 12:26:43 +0100

    Update app.py

--- libs/app.py
+++ libs/app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-counter = 0
-
 @app.route("/")
 def hello():
-    global counter
-    counter += 1
-    return str(counter)
+    return "Hello world!"
 
-app.run(port=5555)
+app.run()
